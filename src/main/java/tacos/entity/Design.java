52d98c957c23d0eba04b45cbc75fbe5802d9094f
chintanallstarz <chintanallstarz@gmail.com>
2018-08-25 12:14:51 -0700

    Added id and createdAt fields and reformatted code

--- src/main/java/tacos/entity/Design.java
+++ src/main/java/tacos/entity/Design.java
@@ -1,25 +1,27 @@
-// tag::all[]
-// tag::allButValidation[]
 package tacos.entity;
-import java.util.List;
-// end::allButValidation[]
+
+import lombok.Data;
+
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-// tag::allButValidation[]
-import lombok.Data;
+import java.sql.Date;
+import java.util.List;
+
 
 @Data
 public class Design {
 
-  // end::allButValidation[]
-  @NotNull
-  @Size(min=5, message="Name must be at least 5 characters long")
-  // tag::allButValidation[]
-  private String name;
-  // end::allButValidation[]
-  @Size(min=1, message="You must choose at least 1 ingredient")
-  // tag::allButValidation[]
-  private List<String> ingredients;
+    private Long id;
+
+    private Date createdAt;
+
+    @NotNull
+    @Size(min = 5, message = "Name must be at least 5 characters long")
+
+    private String name;
+    @Size(min = 1, message = "You must choose at least 1 ingredient")
+
+    private List<String> ingredients;
 
 }
 //end::allButValidation[]
